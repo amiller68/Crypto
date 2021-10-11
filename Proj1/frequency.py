c23f3c422c86136d3a760af3318ce0dc6b7fa3e7
Alex <alexanderscottmiller@gmail.com>
2021-10-10 22:39:47 -0500

    hmmm, chisq tests

--- Proj1/frequency.py
+++ Proj1/frequency.py
@@ -1,6 +1,7 @@
 import string 
 import math 
 import collections as col
+from scipy.stats import chisquare, fisher_exact
 
 # Dictionary of frequency of letters in the English language
 # Retrieved from http://pi.math.cornell.edu/~mec/2003-2004/cryptography/subs/frequencies.html
@@ -54,16 +55,35 @@ def generate_frequency(text):
 	for i in num_alpha:
 		freq_alpha[i] = (num_alpha[i] / total_count) * 100
 
-	sorted_freq = sorted(freq_alpha.items(), key=lambda item: item[1], reverse=True)
-
-	print(sorted_freq)
+	return freq_alpha
 
 
 # Compare two alphanumeric frequencies and return a tuple:
 # 	map
 # where match is a boolean describing whether two frequencies are similar enough
 # and map is a potential frequency mapping
-def map_frequencies(exp_freq, obs_freq):
+# Assumes inputted frequencies normalized to add up to 100.0
+def map_frequencies(obs_freq, exp_freq=None):
+	if exp_freq is None:
+		exp_freq = letterFrequency
+
+	# Sorted tuple lists of expected frequencies, normalized to 100.0
+	sorted_exp_freq = sorted(exp_freq.items(), key=lambda item: item[1], reverse=True)
+	sorted_obs_freq = sorted(obs_freq.items(), key=lambda item: item[1], reverse=True)
+
+	# Problem: gives us p = 1.0
+	chisq, p = chisquare(
+		[v + 0.0001 for (k, v) in sorted_obs_freq],
+		[v + 0.0001 for (k, v) in sorted_obs_freq]
+	)
+
+	if p < 0.05:
+		print("Prime for frequency attack")
+		return dict(
+			zip([k for (k, v) in sorted_obs_freq],[v for (k, v) in sorted_exp_freq])
+		)
+	print("These don't match :(")
+	print(p)
 	return None
 
 
@@ -77,11 +97,11 @@ def replace_letters(text, freq):
 # Returns a best guess at the cipher
 def frequency_analysis(text, v_opt=False):
 	obs_freq = generate_frequency(text)
-	map = map_frequencies(letterFrequency, obs_freq)
+	map = map_frequencies(obs_freq)  #exp_freq = letterFrequency by default
 	if map:
-		best_guess = replace_letters(text, map)
+		text = replace_letters(text, map)
+	return text
 
-	return ""
 
 # Run a detailed frequency analysis on a single file
 if __name__ == "__main__":
@@ -90,22 +110,5 @@ if __name__ == "__main__":
 	ptxtfile = open(filename)
 	ptxt_str = ptxtfile.read()
 
-	# Alphabet dictionary to count frequency of all letters
-	num_alpha = dict.fromkeys(string.ascii_lowercase, 0)
-
-	# Add letters to the alphabet dictionary
-	for i in ptxt_str:
-		if i in num_alpha:
-			num_alpha[i] += 1
-
-	print(num_alpha)
-	total_count = sum(num_alpha.values())
-
-	# Adding percentage frequency of each letter to the dictionary
-	freq_alpha = dict.fromkeys(string.ascii_lowercase, None)
-	for i in num_alpha:
-		freq_alpha[i] = (num_alpha[i] / total_count) * 100
-
-	sorted_freq = sorted(freq_alpha.items(), key=lambda item: item[1], reverse=True)
-
-	print(sorted_freq)
+	best_guess = frequency_analysis(ptxt_str)
+	print(best_guess)
