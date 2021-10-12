01bb029542ce55684e5ee111f5e2a32b0c8e2c5a
Alex <alexanderscottmiller@gmail.com>
2021-10-12 00:45:43 -0500

    counts table size for sus homophonics

--- Proj1/homophonic.py
+++ Proj1/homophonic.py
@@ -4,9 +4,17 @@ def ngram_analysis(ctxt):
     if ctxt.isdecimal():
         if len(ctxt) % 2 == 0:
             best_freq = generate_frequency(ctxt, spacing=2)
-            return True, best_freq
+            best_guess = {
+                'table_size': len(best_freq),
+                'freq': best_freq
+            }
+            return True, best_guess
         if len(ctxt) % 3 == 0:
             best_freq = generate_frequency(ctxt, spacing=3)
-            return True, best_freq
+            best_guess = {
+                'table_size': len(best_freq),
+                'freq': best_freq
+            }
+            return True, best_guess
     return False, ""
 
